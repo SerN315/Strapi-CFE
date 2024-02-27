0847c59d597684deb0e044c8f597a164799f4396
taquan <tnaquan@gmail.com>
2024-02-27 10:40:23 +0700

    - feat: thêm data cho giới thiệu về các ban

---
+++ src/api/gioi-thieu-ban/controllers/gioi-thieu-ban.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gioi-thieu-ban controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::gioi-thieu-ban.gioi-thieu-ban');
